1dfc0e358cd67004cad7d04cfa20ff5045e57250
aveencoder <aveen598@gmail.com>
2025-10-02 20:59:38 +0530

    done for testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import LoginForm from "./LoginForm";
 
 const App = () => {
+  const [isLoggedIn,setLoggedIn] = useState(false);
+   
+  const handlefunction=()=>{
+       setLoggedIn(true);
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{margin:'150px'}}>
+        <h1>Parent Component</h1>
+        {isLoggedIn? (
+          <h2>You are Logged in.</h2>
+        ) : (<LoginForm onLogin={handlefunction}/>)
+        }
     </div>
   )
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function LoginForm({ onLogin}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username && password) {
+      onLogin(); // call parent function
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div style={{marginBottom:'20px'}}>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
+
